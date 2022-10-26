9ee1d48c4ac260709e873709dda506e869c9d1d7
AlessHub <aless312@gmail.com>
2022-10-26 12:42:32 +0200

    made connection between localStorage and roulette random name selection + name removal, also added temporary buttons to move between pages

--- js/script.js
+++ js/script.js
@@ -14,6 +14,12 @@ let submit = document.querySelector('#submit');
 let faceNun = document.querySelector('.faceNun');
 
 
+function retrieveNames() {
+  savedNames = JSON.parse(localStorage.getItem("tasks"));
+}
+retrieveNames();
+console.log(savedNames)
+
 // const randomElement = items[Math.floor(Math.random() * items.length)];
 const killBtnHandler = () => {
   spin();
@@ -43,32 +49,31 @@ roulette.appendChild(newItem);
 // funcion que crea un nombre al azar, cambia el nombre target por ese nombre, y luego lo elimina del array "items"
 
 
-function addName(){
-  boxvalue = document.getElementById('submitText').value;
-  console.log(boxvalue)
-  let objectName = {
-    name: boxvalue
-  }
-  items.push(objectName);
-  console.log(items)
-  return false;
- }
+// function addName(){
+//   boxvalue = document.getElementById('submitText').value;
+//   console.log(boxvalue)
+//   let objectName = {
+//     name: boxvalue
+//   }
+//   items.push(objectName);
+//   console.log(items)
+//   return false;
+//  }
 
 function nameSelect() {
-  let random = items.sort(() => .5 - Math.random()).slice(0, 1)
+  let random = savedNames.sort(() => .5 - Math.random()).slice(0, 1)
   let elem = document.querySelectorAll('span');
   console.log(random)
-  elem[19].innerText = random[0].name
-  items.forEach(function(e){
-    if(e.name == random[0].name){
-        items.splice(items.findIndex(function(i){
-          return i.name === random[0].name;
+  elem[19].innerText = random[0].task
+  savedNames.forEach(function(e){
+    if(e.task == random[0].task){
+        savedNames.splice(savedNames.findIndex(function(i){
+          return i.task === random[0].task;
         }), 1);
-        console.log(items)
-    }
+    }});
     elem[19].classList.add('flip-in-ver-right')
+    console.log(savedNames)
     setTimeout(function(){elem[19].classList.remove('flip-in-ver-right')}, 2000)
-  })
 }
 
 // resetea el nombre del target a "Coder"
@@ -83,6 +88,7 @@ function nameReset() {
 
 function nunAnimation() {
   faceNun.classList.add("scale-up-center");
+  sounds.killSound.play();
   setTimeout(function(){faceNun.classList.remove("scale-up-center")}, 2500)
 }
 
@@ -115,13 +121,19 @@ setTimeout(function(){roulette.classList.remove('slide-right');
 sounds.win.pause();
 currentTime=0;
 }, 7000);
-setTimeout(function(){nunAnimation();
-sounds.killSound.play()}, 7500);
-setTimeout(function(){deathPopUp()}, 8500)
+setTimeout(function(){nunAnimation()}, 7500);
+setTimeout(function(){
+  deathPopUp();
+}, 11000)
 }
 
-// When the user clicks on <div>, open the popup
+// Death
 function deathPopUp() {
+  let elem = document.querySelectorAll('span')
   let popup = document.getElementById("myPopup");
+  popup.innerText = `El/la coder ${elem[19].innerText} ha sido sacrificad@.`
   popup.classList.toggle("show");
+  setTimeout(function(){
+    popup.classList.toggle("show");
+  }, 3000)
 }
